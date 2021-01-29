60385c0abece9ef3b0e5658aefa1f0f8be4cc488
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2021-01-29 15:19:51 +0100

    added establish_connection function

--- src/RemoteGtkREPL.jl
+++ src/RemoteGtkREPL.jl
@@ -12,10 +12,28 @@ module RemoteGtkREPL
     struct GtkREPL_Server
         socket::Sockets.TCPSocket
         console_idx::Int
+        remote_module::String
     end
-    function init(socket, idx)
-        global gtkrepl = GtkREPL_Server(socket, idx)#this is called in remote_console_startup.jl
+    function estalbish_connection(ARGS)
+        gtkrepl_port = parse(Int, ARGS[1])
+        idx = parse(Int, ARGS[2]) # console/worker id
+        remote_module = ARGS[3]     # the module calling us as a String
+
+        port, server = start_server()
+    
+        socket = connect(gtkrepl_port)
+        global gtkrepl = GtkREPL_Server(socket, idx, remote_module)
+
+        remotecall_fetch(include_string, socket, Main,
+            "$(remote_module).add_remote_console_cb($(idx), $(port))"
+        )
+
+        @async begin    
+            read_stdout, wr = redirect_stdout()
+            watch_stdio_task = @async watch_stream(read_stdout, socket, idx, remote_module)
+        end
     end
+
 
     function __init__()
         global _run_task = @async begin end
